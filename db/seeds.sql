7831dd916ad2f3194e7361038912694fab774dac
SBuchanan1 <syd.buch@gmail.com>
2020-10-09 00:34:05 -0400

    changed all cats to burgers

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Cheeseburger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Philly Burger');
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Pizza Burger', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Big Bleu Burger', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Bacon Cheddar', true);
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Turkey Burger');
