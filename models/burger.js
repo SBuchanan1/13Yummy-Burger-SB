950e04d406343129a6923591a34d57fb0defd456
SBuchanan1 <syd.buch@gmail.com>
2020-11-18 15:05:14 -0500

    revised structure of doc to match the correct activity

--- models/burger.js
+++ models/burger.js
@@ -8,18 +8,18 @@ var burger = {
     });
   },
   // The variables cols and vals are arrays.
-  create: function (cols, vals, cb) {
-    orm.create("burgers", cols, vals, function (res) {
+  create: function (name, cb) {
+    orm.insertOne(name, cb, function (res) {
       cb(res);
     });
   },
-  update: function (objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function (res) {
+  update: function (id, cb) {
+    orm.update(id, cb, function (res) {
       cb(res);
     });
   },
-  delete: function (condition, cb) {
-    orm.delete("burgers", condition, function (res) {
+  delete: function (id, cb) {
+    orm.delete(id, cb, function (res) {
       cb(res);
     });
   }
